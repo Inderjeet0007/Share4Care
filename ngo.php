6a5904cc5b410ee6318729beea713c9ec57eeb4a
Inderjeet Saluja <32186872+Inderjeet0007@users.noreply.github.com>
2018-03-10 23:56:18 +0530

    Add files via upload

---
+++ ngo.php
@@ -0,0 +1,22 @@
+<?php 
+session_start(); 
+
+if (!isset($_SESSION['username'])) {
+$_SESSION['msg'] = "You must log in first";
+header('location: login_ngo.php');
+}
+if (isset($_GET['logout'])) {
+session_destroy();
+unset($_SESSION['username']);
+header("location: login_ngo.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<?php if (isset($_SESSION['username'])) : ?>
+    <p>Welcome, <strong><?php echo $_SESSION['username']; ?></strong></p>
+    <p> <a href="login_admin.php?logout='1'" style="color: red;">logout</a> </p>
+<?php endif ?>
+</body>
+</html>
